69dc4d78b894eef87c30ac178d66ef1dc2b228a5
Jean Urena <juanpedro1519@gmail.com>
2017-08-26 17:30:54 -0400

    [flask-megat] - Part2 -> Templates

--- flask-megatutorial/microblog/app/views.py
+++ flask-megatutorial/microblog/app/views.py
@@ -1,9 +1,26 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World"
+	# fake user
+	user = {"nickname": "jean"}
+	# fake posts
+	posts = [
+		{ 
+			'author': {"nickname" : "Jhon"},
+			'body': "Beautiful Day in Portland"
+		},
+		{
+			'author': {"nickname": "Susan"},
+			'body': "The avengers movie was so cool"
+		}
+	]
+	
+	return render_template('index.html', 
+							user=user, 
+							posts=posts)
 
 @app.route('/tester')
 def tester():
