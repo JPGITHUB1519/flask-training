769a7886ce47f5d643d31c590248e50b8d1cf36f
Jean Urena <juanpedro1519@gmail.com>
2017-08-27 15:53:50 -0400

    [flask-mega-t] - Part4 -> Database

--- flask-megatutorial/microblog/config.py
+++ flask-megatutorial/microblog/config.py
@@ -1,2 +1,11 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Flask-WTF 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+
+# Flask-SQLAlchemy path of the database
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# folder where we will store the SQLAlchemy-migrate data files
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
