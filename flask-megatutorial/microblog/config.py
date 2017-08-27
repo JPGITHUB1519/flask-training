67bc0c7f5967a8f1cc5456cf4fe76cff87eaeaff
Jean Urena <juanpedro1519@gmail.com>
2017-08-27 16:58:23 -0400

    [flask-megat-extra] - Implement flask-migrate instead sqlalchemy-migrate

--- flask-megatutorial/microblog/config.py
+++ flask-megatutorial/microblog/config.py
@@ -8,4 +8,3 @@ SECRET_KEY = 'you-will-never-guess'
 # Flask-SQLAlchemy path of the database
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 # folder where we will store the SQLAlchemy-migrate data files
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
