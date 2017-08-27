4ba7606c2f0e0f5344a47fcb36eda751945adea4
Jean Urena <juanpedro1519@gmail.com>
2017-08-26 21:42:30 -0400

    [flask-mega-t] - Part3 -> Web Forms

--- flask-megatutorial/microblog/app/views.py
+++ flask-megatutorial/microblog/app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -25,3 +26,14 @@ def index():
 @app.route('/tester')
 def tester():
 	return "Tester Route"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form =  LoginForm()
+	# returns false for get request, returns true when all data is correct
+	if form.validate_on_submit():
+		flash('Login Requested for username=%s' % (form.username.data))
+		return redirect('/index')
+	return render_template('login.html',
+							title="Log In",
+							form=form)
