9378318bb730a5d472bd8e8a2027625381e107d1
Jean Urena <juanpedro1519@gmail.com>
2017-08-28 23:03:42 -0400

    [flask-megat-book] - Sending Mails

--- flask-megatutorial/microblog/app/views.py
+++ flask-megatutorial/microblog/app/views.py
@@ -1,6 +1,25 @@
 from flask import render_template, flash, redirect
-from app import app
+from flask_mail import Message
+from app import app, mail
 from .forms import LoginForm
+from threading import Thread
+
+def send_async_email(app, msg):
+	with app.app_context():
+		mail.send(msg)
+
+def send_email(to, subject, template, **kwargs):
+	"""
+	Send Asyncronous mail
+	"""
+	msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+				sender=app.config['FLASKY_MAIL_SENDER'],
+				recipients=[to])
+	msg.body = render_template(app.config['FLASKY_MAIL_TEMPLATES_FOLDER'] +  '/' + template + '.txt', **kwargs)
+	msg.html = render_template(app.config['FLASKY_MAIL_TEMPLATES_FOLDER'] +  '/' + template + '.html', **kwargs) 	
+	thr = Thread(target=send_async_email, args=[app, msg])
+	thr.start()
+	return thr
 
 @app.route('/')
 @app.route('/index')
@@ -37,3 +56,9 @@ def login():
 	return render_template('login.html',
 							title="Log In",
 							form=form)
+
+@app.route('/mail')
+def send_mail():
+	send_email('juanpedrotramposo@gmail.com', 'Welcome', 'mail', user="jean")
+	send_email('juanpedrotramposo@gmail.com', 'Welcome', 'mail', user="jean")
+	return "hey"
--- flask-megatutorial/microblog/config.py
+++ flask-megatutorial/microblog/config.py
@@ -7,4 +7,18 @@ SECRET_KEY = 'you-will-never-guess'
 
 # Flask-SQLAlchemy path of the database
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-# folder where we will store the SQLAlchemy-migrate data files
+
+# mail
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+# never write your credentials in the scripts instead in environment variables
+# MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+# MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+MAIL_USERNAME = "juanpedrotramposo@gmail.com"
+MAIL_PASSWORD = "23051519"
+# custom mail settings
+FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKY]'
+FLASKY_MAIL_SENDER = 'Flask Admin <juanpedrotramposo@gmail.com>'
+FLASKY_MAIL_TEMPLATES_FOLDER = '/mail'
+
