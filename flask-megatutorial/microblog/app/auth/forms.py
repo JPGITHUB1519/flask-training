0a398549e8329bd68d03090d3c947314d0b93497
Jean Urena <juanpedro1519@gmail.com>
2017-09-01 22:36:00 -0400

    [flask-megat-book] - Signing u p users

--- flask-megatutorial/microblog/app/auth/forms.py
+++ flask-megatutorial/microblog/app/auth/forms.py
@@ -1,9 +1,29 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Email, Length
+from wtforms.validators import Required, Email, Length, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 class LoginForm(Form):
 	email = StringField('Email', validators=[Required(), Length(1, 120), Email()])
 	password = PasswordField('Password', validators=[Required()])
 	remember_me = BooleanField('Keep me logged in')
 	submit = SubmitField('Log In')
+
+class RegistrationForm(Form):
+	email = StringField('Email', validators=[Required(), Length(1, 120), Email()])
+	nickname = StringField('Username', validators=[Required(), Length(1,64), 
+		Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, ''numbers, dots or underscores')])
+	password = PasswordField('Password', validators=[Required(), EqualTo('password2', message="Password must match.")])
+	password2 = PasswordField('Confirm Password', validators=[Required()])
+	submit = SubmitField('Register')
+
+	# customs validators
+	# when we define validate_<name_of_field> we apply a new custom validator to that field
+	def validate_email(self, field):
+		if User.query.filter_by(email=field.data).first():
+			raise ValidationError('Email already registered.')
+
+	def validate_nickname(self, field):
+		if User.query.filter_by(nickname=field.data).first():
+			raise ValidationError('Username already in use')
