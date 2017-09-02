d05a3ad21ecc9e2f901161c82202f8ae3e29725b
Jean Urena <juanpedro1519@gmail.com>
2017-09-01 23:39:46 -0400

    [flask-megat-book] - user confirmation

--- flask-megatutorial/microblog/app/auth/views.py
+++ flask-megatutorial/microblog/app/auth/views.py
@@ -1,10 +1,18 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
+from ..emails import send_email
 
+
+# handling unconfirmed users
+# auth.before_app_request works in the whole aplication, auth.before_request only works for the current blueprint
+@auth.before_app_request
+def before_request():
+	if current_user.is_authenticated and not current_user.confirmed and request.endpoint[:5] != 'auth.':
+		return redirect(url_for('auth.unconfirmed'))
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,6 +29,7 @@ def login():
 	return render_template('/auth/login.html', form=form) 	
 
 @auth.route('/logout')
+@login_required
 def logout():
 	logout_user()
 	flash('You have been logged out')
@@ -35,7 +44,27 @@ def register():
 					password=form.password.data)
 		db.session.add(user)
 		db.session.commit()
-		flash('You can now login')
-		return redirect(url_for('auth.login'))
+		token = user.generate_confirmation_token()
+		send_email(user.email, 'Confirm your account', '/auth/email/confirm', user=user, token=token)
+		flash('A confirmation email has been sent to you by email')
+		return redirect(url_for('main.index'))
 
 	return render_template('auth/register.html', form=form)
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+	if current_user.confirmed:
+		return redirect(url_for('main.index'))
+	if current_user.confirm(token):
+		flash('You have confirmed your account, Thanks!')
+	else:
+		flash('The confirmation link in invalid or has expired')
+
+	return redirect(url_for('main.index'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+	if current_user.is_anonymous or current_user.confirmed:
+		return redirect('main.index')
+	return render_template('auth/unconfirmed.html')
--- flask-megatutorial/microblog/app/models.py
+++ flask-megatutorial/microblog/app/models.py
@@ -1,4 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from flask_login import UserMixin
 from app import db
 from . import login_manager
@@ -10,6 +12,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
 
     # read only property password
     @property
@@ -26,6 +29,27 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+    # user validations methods
+    def generate_confirmation_token(self, expiration=3600):
+        """ Generate a new encrypted confirmation token"""
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm' : self.id})
+
+    def confirm(self, token):
+        """ Confirm user token to activate user"""
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except Exception as e:
+            return False
+
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        db.session.commit()
+        return True 
 
 
 class Post(db.Model):
