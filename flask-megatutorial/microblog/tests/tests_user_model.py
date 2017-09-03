d774ba64091f339c2e350f48e4d6e75599fbd97b
Jean Urena <juanpedro1519@gmail.com>
2017-09-03 16:53:11 -0400

    [flask-megat-book] - Adding setup and teardown for usermodel tests

--- flask-megatutorial/microblog/tests/tests_user_model.py
+++ flask-megatutorial/microblog/tests/tests_user_model.py
@@ -4,7 +4,23 @@ from app import db
 from app.models import User
 
 class UserModelTestCase(unittest.TestCase):
+	def setUp(self):
+		"""
+		Create application for testing and activates its context
+		"""
+		self.app = create_app('testing')
+		self.app_context = self.app.app_context()
+		self.app_context.push()
+		db.create_all()
 
+	def tearDown(self):
+		"""
+		Remove application context and database
+		"""
+		db.session.remove()
+		db.drop_all()
+		self.app_context.pop()
+		
 	def test_password_setter(self):
 		""" test password_hash is setted automatically"""
 		u = User(password='cat')
